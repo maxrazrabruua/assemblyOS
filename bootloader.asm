902e9e02fed8dbe0894146b26cf44c68dcd244ed
Ардиновец <149672990+maxrazrabruua@users.noreply.github.com>
2025-04-11 14:57:22 +0300

    Add files via upload

---
+++ bootloader.asm
@@ -0,0 +1,41 @@
+BITS 16
+ORG 0x7C00
+start:
+xor ax,ax
+mov ds,ax
+mov es,ax
+mov si,msg
+call l
+l:
+mov ah,0x02
+mov dl,0x80
+mov es,0x8000
+mov cx,10
+mov bx,1
+call r
+r:
+mov al,0x01
+mov ch,0
+mov cl,bl
+mov dn,0
+mov di,0
+int 0x13
+jc e
+add bx,1
+loop r
+p:
+mov ah,0x0E
+.n:
+lodsb
+cmp al,0
+je .d
+int 0x10
+jmp .n
+.d:
+ret
+e:
+mov si,i
+call p
+i db "BE: 0",0
+times 510-($-$$) db 0
+dw 0xAA55
