edcf84ac6a514e969aa135211f648301a2af1ee4
Ардиновец <149672990+maxrazrabruua@users.noreply.github.com>
2025-04-11 16:12:15 +0300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,43 +1,43 @@
 BITS 16
 ORG 0x7C00
 start:
-xor ax,ax
-mov ds,ax
-mov es,ax
-call l
-mov ax,0x8000
-jmp 0x8000:0000
+ xor ax,ax
+ mov ds,ax
+ mov es,ax
+ call l
+ mov ax,0x8000
+ jmp 0x8000:0000
 l:
-mov ax,0x8000
-mov ah,0x02
-mov dl,0x80
-mov cx,12
-mov bx,2
-call r
+ mov ax,0x8000
+ mov ah,0x02
+ mov dl,0x80
+ mov cx,12
+ mov bx,2
+ call r
 r:
-mov al,0x01
-mov ch,0
-mov cl,bl
-mov dh,0
-mov di,0
-int 0x13
-jc e
-inc bx
-loop r
-ret
+ mov al,0x01
+ mov ch,0
+ mov cl,bl
+ mov dh,0
+ mov di,0
+ int 0x13
+ jc e
+ inc bx
+ loop r
+ ret
 e:
-mov si,i
-call p
+ mov si,i
+ call p
 p:
-mov ah,0x0E
-.n:
-lodsb
-cmp al,0
-je .d
-int 0x10
-jmp .n
-.d:
-ret
+ mov ah,0x0E
+ .n:
+ lodsb
+ cmp al,0
+ je .d
+ int 0x10
+ jmp .n
+ .d:
+ ret
 i db "BE: 0",0
-times 510-($-$$) db 0
-dw 0xAA55
+ times 510-($-$$) db 0
+ dw 0xAA55
