124f6e3fd476bb3fab010d1faa48f3e0e21ae09d
Ардиновец <149672990+maxrazrabruua@users.noreply.github.com>
2025-04-11 15:33:09 +0300

    Create system.asm

---
+++ system.asm
@@ -0,0 +1,25 @@
+; Система, должна весить менее 5 кб
+
+start:
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov ah,0x0E
+.next:
+    lodsb
+    cmp al,0
+    je .d
+    int 0x10
+    jmp .next
+.done:
+    ret
+
+newline:
+    mov ah, 0x02
+    inc dh
+    int 0x10
+
+message db "Hello world!", 0
+times 5120-($-$$) db 0
