fd2d5dfa036ace3a265ba46c3e15eb37504d270e
Ардиновец <149672990+maxrazrabruua@users.noreply.github.com>
2025-04-11 17:38:17 +0300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -4,16 +4,17 @@ start:
  xor ax,ax
  mov ds,ax
  mov es,ax
+ mov si,t
+ call p
  call l
- mov ax,0x8000
- jmp 0x8000:0000
 l:
- mov ax,0x8000
  mov ah,0x02
  mov dl,0x80
  mov cx,12
  mov bx,2
  call r
+ mov ax,0x8000
+ jmp 0x8000:0000
 r:
  mov al,0x01
  mov ch,0
@@ -30,14 +31,16 @@ e:
  call p
 p:
  mov ah,0x0E
- .n:
+.n:
  lodsb
  cmp al,0
  je .d
  int 0x10
  jmp .n
- .d:
+.d
  ret
+
 i db "BE: 0",0
- times 510-($-$$) db 0
- dw 0xAA55
+t db "BOOT: runing...",0
+times 510-($-$$) db 0
+dw 0xAA5
